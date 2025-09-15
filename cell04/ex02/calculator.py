615c95a583de409e468fec847d1f33fa4ad94716
IamTubbe <67030298@kmitl.ac.th>
2025-09-16 03:07:33 +0700

    ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+age_count = int(input("Please tell me your age: "))
+print(f"You are currently {age_count} year old.")
+print(f"In 10 years, you'll be {age_count + 10} years old.")
+print(f"In 20 years, you'll be {age_count + 20} years old.")
+print(f"In 30 years, you'll be {age_count + 30} years old.")
