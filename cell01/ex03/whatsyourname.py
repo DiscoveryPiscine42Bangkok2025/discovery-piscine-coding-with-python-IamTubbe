e64a717447c33cc2162de39968e3daaa6a16d262
IamTubbe <67030298@kmitl.ac.th>
2025-09-15 22:42:14 +0700

    ex03

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-text = input("Hey, what's your first name? : ").strip()
-text2 = input("And your last name? : ").strip()
+text = input("Hey, what's your first name? : ")
+text2 = input("And your last name? : ")
 print(f"Well , pleased to meet you , {text} {text2}.")
