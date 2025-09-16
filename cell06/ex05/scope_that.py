3cb6326452e44596b1e95ae6ad988a8313fff4fc
IamTubbe <67030298@kmitl.ac.th>
2025-09-16 21:57:46 +0700

    ex05

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add_one(x):
+    return x + 1
+
+def main():
+    num = 5
+    print("Before calling add_one :", num)
+
+    num = add_one(num)
+    print("After calling add_one :", num)
+
+if __name__ == "__main__":
+    main()
