b1f92c6ef49be9d1f8552642310f1ab97535bc1e
IamTubbe <67030298@kmitl.ac.th>
2025-09-16 20:31:10 +0700

    ex02

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
-age_count = int(input("Please tell me your age: "))
-print(f"You are currently {age_count} year old.")
-print(f"In 10 years, you'll be {age_count + 10} years old.")
-print(f"In 20 years, you'll be {age_count + 20} years old.")
-print(f"In 30 years, you'll be {age_count + 30} years old.")
+
+num1 = input("Give me the first number: ")
+num2 = input("Give me the second number: ")
+
+num1 = float(num1)
+num2 = float(num2)
+
+print("Thank you!")
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
