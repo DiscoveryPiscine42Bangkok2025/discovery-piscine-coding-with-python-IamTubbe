769b183700f9b2e10a33de3ef72446bab7e981d2
IamTubbe <67030298@kmitl.ac.th>
2025-09-16 15:29:53 +0700

    ex09

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 import sys
-import re
 
 if len(sys.argv) != 3:
     print("none")
 else:
-    keyword = sys.argv[1]
-    string = sys.argv[2]
-    occurrences = re.findall(rf"{re.escape(keyword)}", string)
-    if occurrences:
-        print(len(occurrences))
-    else:
-        print("none")
+    word = sys.argv[1]
+    text = sys.argv[2]
+
+    count = text.count(word)
+    print(count if count > 0 else "none")
